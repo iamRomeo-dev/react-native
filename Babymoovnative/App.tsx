38496c9654b6beb56e303764f1233854aba83e04
iamRomeo-dev <romeo.brilland@live.fr>
2023-02-09 09:54:02 +0100

    feat: :sparkles: Begin new library

--- Babymoovnative/App.tsx
+++ Babymoovnative/App.tsx
@@ -77,19 +77,10 @@ function App(): JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            <Text style={styles.highlight}>
+              React native ble manager library
+            </Text>
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
